28e62e50bad6dc05c8d3ce4065e8017eaf44a702
Habil Yadigar <46094018+habilyadigar@users.noreply.github.com>
2020-11-01 23:24:39 +0300

    Director validations

--- models/Director.js
+++ models/Director.js
@@ -3,9 +3,14 @@ const Schema = mongoose.Schema;
 
 
 const DirectorSchema = new Schema({
-    name : String,
-    surname: String,
-    bio: String,
+    name : {
+        type : String,
+        required : [true,'`{PATH}` alanı zorunludur.'],
+        maxlength: [20,'`{PATH}` alanı (`{Value}`),({MAXLENGTH} karakterden küçük olmalıdır.)'],
+        minlength: [1,'`{PATH}` alanı (`{Value}`),({MINLENGTH} karakterden büyük olmalıdır.)'] 
+    },
+    surname: {type: String, maxlength:50,minlength:2},
+    bio: {type: String,maxlength:500,minlength:10},
     createdAt: {
         type: Date,
         default: Date.now
