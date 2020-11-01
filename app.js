4677b51ed730385502d55fe42a6d39529f0a2b2e
Habil Yadigar <46094018+habilyadigar@users.noreply.github.com>
2020-11-01 18:27:27 +0300

    db-post-get-delete-put
    db schemas and api operations added

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const movie = require('./routes/movie');
 
 const app = express();
+mongoose.connect('mongodb://localhost/Filmoloji')
+  .then(()=>{
+    console.log('Mongodb bağlantı sağlandı');
+  })
+  .catch((err)=>{
+    console.log(err+'mongodb bağlantı hatası!!!');
+  });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,12 +24,12 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/movies', movie);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) =>{
---
+++ routes/movie.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const router = express.Router();
+
+const Movie = require('../models/Movie');
+
+router.get('/',(req, res)=>{
+  const promise = Movie.find({ });
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err)
+  });
+});
+
+router.delete('/:movie_id',(req, res,next) => {
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+
+  promise.then((movie)=>{
+    if(!movie)
+      next({message : 'The movi was not found.',code:99});
+    res.json(movie);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+//Top 10
+router.get('/top10',(req, res)=>{
+  const promise = Movie.find({ }).limit(10).sort({imdb_score: -1});
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err)
+  });
+});
+//between
+router.get('/between/:start_year/:end_year',(req, res)=>{
+  const  {start_year,end_year} = req.params;
+  const promise = Movie.find(
+  { 
+    year:{ "$gte": parseInt(start_year), "$lte":parseInt(end_year)}
+  }
+  );
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err)
+  });
+});
+
+router.get('/:movie_id',(req, res) => {
+  const promise = Movie.findById(req.params.movie_id);
+
+  promise.then((movie)=>{
+    res.json(movie);
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+
+router.put('/:movie_id',(req, res) => {
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {new:true});
+
+  promise.then((movie)=>{
+    res.json(movie);
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+
+
+router.post('/', (req, res, next)=> {
+  //const {title,imdb_score,category,year} = req.body; // destructing
+  const movie = new Movie(req.body);
+/*
+  const movie = new Movie({
+    title: title,
+    imdb_score:imdb_score,
+    category : category,
+    year : year
+  });
+  */
+  movie.save((err,data)=>{
+    if(err)
+      res.json(err);
+    res.json(data)
+  });
+  movie.save((err,data)=>{
+    if(err)
+      res.json(err);
+    res.json(data);
+  });
+/*
+  const promise = movie.save();
+  promise.then ((data)=>{
+    res.json({status : 1});
+  })
+  .catch((err)=>{res.json(err)
+  });
+*/
+});
+
+
+
+
+
+
+
+module.exports = router;
