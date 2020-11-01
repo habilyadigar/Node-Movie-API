edbb22e4190c9b9e73df8c789e709285484f1a87
Habil Yadigar <46094018+habilyadigar@users.noreply.github.com>
2020-11-01 22:07:38 +0300

    director and movies merged with inner join

--- routes/director.js
+++ routes/director.js
@@ -6,24 +6,83 @@ const router = express.Router();
 const Director = require('../models/Director');
 
 
+router.get('/',(req, res)=>{
+  const promise = Director.aggregate([
+    {
+      $lookup:{
+        from :'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as : 'movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    
+    {
+      $group: {
+        _id:{
+          _id: '$id',
+          name: '$name',
+          surname: '$surname',
+          bio : '$bio'
+        },
+        movies: {
+          $push : '$movies'
+        }
+      }
+    },
+    
+    {
+      $project:{
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movies'
+      }
+    }
+    
+]);
 
-router.get('/', (req, res, next) =>{
-  res.json({ title: 'directors' });
+router.get('/',(req, res)=>{
+  const promise = Director.find({ });
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err)
+  });
 });
+
+
+
+promise.then((data) =>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
 
 router.post('/', (req, res, next) =>{
   const director = new Director(req.body);
   const promise = director.save();
 
-
   promise.then((data)=>{
     res.json(data);
   }).catch((err)=>{
-    res.json(data);
+    res.json(err);
   });
-
-
 });
+
+
+
+
+
+
 
 
 module.exports = router;
